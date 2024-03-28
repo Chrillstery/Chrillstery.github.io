c7f28b6130c55efde57273bd7101983b43aa476f
Chrillstery <1513517648@qq.com>
2024-03-28 23:29:20 +0800

    Site updated: 2024-03-28 23:29:20

--- styles/background.js
+++ styles/background.js
@@ -1,11 +1,11 @@
 //随机背景图片数组,图片可以换成图床链接，注意最后一条后面不要有逗号
 var backimg =[
-  "url(/image/girl.jpg)",
-  "url(/image/night.jpg)",
-  "url(/image/knight.jpg)",
-  "url(/image/background.jpg)",
-  "url(/image/plain.jpg)",
-  "url(/image/mountain.jpg)"
+  "url(https://img2.imgtp.com/2024/03/28/iGOm8tKH.jpg)",
+  "url(/image/https://img2.imgtp.com/2024/03/28/2LL6R5OS.jpg)",
+  "url(https://img2.imgtp.com/2024/03/28/3kV6yXd1.jpg)",
+  "url(https://img2.imgtp.com/2024/03/28/OCDAolKo.jpg)",
+  "url(https://img2.imgtp.com/2024/03/28/xuK6ReuV.jpg)",
+  "url(https://img2.imgtp.com/2024/03/28/st4AMPCl.jpg)"
 ];
 //获取背景图片总数，生成随机数
 var bgindex =Math.floor(Math.random() * (backimg.length));
